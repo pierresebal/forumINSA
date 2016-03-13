d7bd30ee5362395149a88321295f79a3bdf2fcf5
Pierre <petersg83@@gmail.com>
2016-03-13 23:06:30 +0100

    ajout variable session

--- api/controllers/StudentController.js
+++ api/controllers/StudentController.js
@@ -14,22 +14,36 @@ module.exports = {
 
     request.post({url:'https://etud.insa-toulouse.fr/~pnoel/fie/connection.php', form:data}, function(err, httpResponse, body) {
       if (!err && httpResponse.statusCode == 200) {
-        var result = JSON.parse(body);
+        if (body != '0') {
+          var result = JSON.parse(body);
 
-        Student.findOne({login:result.login}).exec(function(err, record) {
-          if (err)
-            return res.negotiate(err);
+          Student.findOne({login: result.login}).exec(function (err, record) {
+            if (err)
+              return res.negotiate(err);
 
-          if (!record) {
-            console.log("A besoin d'être créé : " + result.login);
-            Student.create(result).exec(function(err, created) {
-              console.log("A été créé");
-            });
-          }
-          else
-            console.log("Il y est :)");
-        });
-
+            if (!record) {
+              console.log("A besoin d'être créé : " + result.login);
+              Student.create(result).exec(function (err, created) {
+                console.log("A été créé");
+                req.session.login = created.login;
+                req.session.firstname = created.firstName;
+                req.session.authenticated = true;
+                console.log("login : " + created.login + " " + req.session.login);
+              });
+            }
+            else
+              console.log("Il y est :)");
+            req.session.login = record.login;
+            req.session.firstname = record.firstName;
+            req.session.authenticated = true;
+            console.log("login : " + record.login + " " + req.session.login + " " + req.session.authenticated);
+          });
+        } else {
+          req.session.tried = true;
+          req.session.authenticated = false;
+          console.log("authentication failed.")
+        }
+        return res.redirect("/");
       }
       else
         console.log("erreur : " + err);
--- api/policies/sessionAuth.js
+++ api/policies/sessionAuth.js
@@ -9,8 +9,9 @@
  */
 module.exports = function(req, res, next) {
 
-  // User is allowed, proceed to the next policy, 
+  // User is allowed, proceed to the next policy,
   // or if this is the last policy, the controller
+  console.log("Session authenticated (policy): "+req.session.authenticated);
   if (req.session.authenticated) {
     return next();
   }
