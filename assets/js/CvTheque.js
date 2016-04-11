f6ec5f953c27bbf388e09ed5c20edd9bd1a1591d
Petersg83 <petersg83@gmail.com>
2016-04-11 17:21:32 +0200

    Mise en place react

---
+++ assets/js/CvTheque.js
@@ -0,0 +1,12 @@
+
+var CvTheque = React.createClass({
+  render : function () {
+    return (
+      <div className="CvTheque">
+        <h1>CVThèque</h1>
+        <p>Je suis tous les CVs</p>
+      </div>
+    );
+  }
+});
+ReactDOM.render(<CvTheque />, document.getElementById('subBody'));
