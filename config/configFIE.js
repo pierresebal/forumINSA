0109fdb5a0fd5807dc532a8a1af89c7a3c83f70a
Pieha <pierre.hardy5@gmail.com>
2016-04-30 22:34:45 +0200

    Ajout d'une fonction mail globale dans api/services/SendMail.js (exemple d'utilisation en commentaire dans la fonction) Ajout d'un fichier de configuration dans confi/configFIE.js

---
+++ config/configFIE.js
@@ -0,0 +1,11 @@
+// Config file for global config variables
+
+module.exports.configFIE = {
+
+  // Email parameters
+  FIEmailService : "Gmail",
+  FIEmailAddress : "pierre.hardy5@gmail.com",
+  FIEmailPassword : "Ycg684-Z",
+  FIEmailSenderName : "Forum INSA Entreprises",
+    
+}
