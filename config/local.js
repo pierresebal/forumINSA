1f30bde78e37d7047493f2d557b1b1faa3e3e5ef
Pierre NOEL <petersg83@gmail.com>
2016-06-23 23:23:30 +0200

    encore un merge

--- config/local.js
+++ config/local.js
@@ -61,7 +61,7 @@ module.exports = {
    ***************************************************************************/
 
   // port: process.env.PORT || 1337,
-
+  port: 8080,
   /***************************************************************************
    * The runtime "environment" of your Sails app is either typically         *
    * 'development' or 'production'.                                          *
